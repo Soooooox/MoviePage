b2fcdb4683a9f183cc39006eb6b075fab6ecd8ff
Soooooox <Q2820854831@gmail.com>
2024-02-04 14:50:41 +0800

    create Movie Page

---
+++ README.md
@@ -0,0 +1,6 @@
+Sooox's Book-Page  
+
+> **参考**：
+>
+> 1. https://www.youtube.com/watch?v=b7eJQSHhuO8
+> 2. https://www.youtube.com/watch?v=nu_pCVPKzTk
---
+++ scripts/playlet.js
@@ -0,0 +1,6 @@
+const homebutton = document.getElementById('home-button');
+
+homebutton.addEventListener(
+  'click', () =>{
+    window.location.href = "https://soooooox.github.io/HomePage/"
+  })
